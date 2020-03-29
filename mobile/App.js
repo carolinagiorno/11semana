9403157a85495e5cffb3b2cf112d2eff2345bd91
carolinagiorno <chagiorno@gmail.com>
2020-03-29 17:07:41 -0300

    Dia 4: Mobile - configurações

---
+++ mobile/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Te amo mãe</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
